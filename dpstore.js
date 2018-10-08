54d9bb00064b96a11bc1d1d4eacf83c246a57ca7
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-08 14:42:02 +0200

    new class dpstore
    dpstore

--- dpstore.js
+++ dpstore.js
@@ -1,3 +1,131 @@
+class DepartmentStore extends THREE.Group {
+
+  init (){
+
+      var mDepartment = this;
+
+      //city mesh
+      var townGeometry = new THREE.Geometry();
+
+      var depgeometry = new THREE.BoxGeometry(60, 100, 60);
+      var depMaterials = [
+              new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //LEFT
+              new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //RIGHT
+              new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //TOP
+              new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //BOTTOM
+              new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //FRONT
+              new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //BACK
+          ];
+      var depMaterial = new THREE.MeshFaceMaterial(depMaterials);
+      var meshDep = new THREE.Mesh(depgeometry, depMaterial);
+      mDepartment.add(meshDep);
+
+      //loop
+var dp = 1;
+while (dp < 13)
+  {
+
+if (dp ==1){
+var dpcol =5;
+var dprow =10;
+}
+else if (dp ==2){
+var dpcol =10;
+var dprow =15;
+}
+else if (dp ==3){
+var dpcol =20;
+var dprow =20;
+}
+else if (dp ==4){
+var dpcol =25;
+var dprow =25;
+}
+else if (dp ==5){
+var dpcol =21;
+var dprow =20;
+}
+else if (dp ==6){
+var dpcol =20;
+var dprow =21;
+}
+else if (dp ==7){
+var dpcol =17;
+var dprow =18;
+}
+else if (dp ==8){
+var dpcol =9;
+var dprow =15;
+}
+else if (dp ==9){
+var dpcol =47;
+var dprow =8;
+}
+else if (dp ==10){
+var dpcol =29;
+var dprow =30;
+}
+else if (dp ==11){
+var dpcol =29;
+var dprow =32;
+}
+else if (dp ==12){
+var dpcol =38;
+var dprow =51;
+}
+else {
+var dpcol =1;
+var dprow =1;
+}
+
+// put a position
+meshDep.position.x = (2700 - 5400) + (dpcol * 90) - 30;
+meshDep.position.z = (2700 - 5400) + (dprow * 90) - 30;
+//put a rotation
+meshDep.rotation.y = 0.5*Math.PI*2;
+//building scale
+//buildMesh2.scale.x = 60;
+//buildMesh2.scale.y = 100;
+//buildMesh2.scale.z = buildMesh2.scale.x;
+
+//add geometry
+var depgeometry = meshDep.depgeometry;
+
+THREE.GeometryUtils.merge( townGeometry, meshDep );
+//THREE.Geometry.merge( townGeometry, meshDep );
+
+//increment loop
+dp ++;
+
+   }
+
+//generate material
+var material3 = new THREE.MeshLambertMaterial({
+  color: 0xd7dadf
+  });
+
+//return mesh
+var mesh2 = new THREE.Mesh(townGeometry, material3);
+return mesh2;
+      
+  }
+          
+  constructor(){
+      super();
+
+      this.init();
+  }
+
+}
+
+
+
+
+
+
+
+
+/*
 var THREEx = THREEx || {}
 THREEx.ProceduralDepStore = function(){
 
@@ -139,3 +267,4 @@ return d;
 
 
 }
+*/
