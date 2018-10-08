bb808aa5d29ff262ceeb30e376bcf23fa70acca0
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-08 18:34:20 +0200

    test depot werkt wel
    test depot werkt wel

---
+++ Depot.js
@@ -0,0 +1,30 @@
+class Depot extends THREE.Group {
+
+    init(){
+        var mdepot = this;
+        var dgeometry = new THREE.BoxGeometry(3.9, 13.3, 5.9);
+        var dcubeMaterials = [
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //LEFT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //RIGHT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bobdebouwer.png"), side: THREE.DoubleSide }), //TOP
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //BOTTOM
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bobdebouwer.png"), side: THREE.DoubleSide }), //FRONT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //BACK
+            ];
+        var dmaterial = new THREE.MeshFaceMaterial(dcubeMaterials);
+        var ddepot = new THREE.Mesh(dgeometry, dmaterial);
+        ddepot.position.x = 7;
+        ddepot.position.y = 0;
+        ddepot.position.z = 7;
+        mdepot.add(ddepot);   
+    }
+
+    
+    constructor(){
+        super();
+        //this._LoadState = LoadStates.NOT_LOADING;
+        this.init();
+    }
+    
+    
+}
