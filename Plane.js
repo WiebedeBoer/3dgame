1dbf635d6a220bc00375f09d5cc0c8f3c39cca30
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-08 17:24:21 +0200

    plane color, build remove, bad skybox
    plane color altered, buildings removed, skybox doesn't work yet

--- Plane.js
+++ Plane.js
@@ -1,10 +1,13 @@
 class Plane extends THREE.Group {
 	init(){
                 var mPlane = this;
-                var material        = new THREE.MeshBasicMaterial({ color:0x808080 })
+                var material        = new THREE.MeshBasicMaterial({ color:0x008000 })
                 var geometry        = new THREE.PlaneGeometry( 7430, 7430 )
                 var plane        = new THREE.Mesh( geometry, material );
                 plane.rotation.x= - 90 * Math.PI / 180;
+                //plane.rotation.x = Math.PI / 2.0;
+                //plane.position.x = 15;
+                //plane.position.z = 15;
                 mPlane.add(plane);
     }
     constructor(){
