69ec7696449372852d13c41ffa572d530d8c0aca
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-08 12:40:14 +0200

    skybox and plane ref in index
    skybox and plane ref in index

--- Skybox.js
+++ Skybox.js
@@ -4,7 +4,7 @@ class Skybox extends THREE.Group {
 				var imagePrefix = "images/skybox";
 	            var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
 	            var imageSuffix = ".png";
-	            var skyGeometry = new THREE.CubeGeometry( 1000, 1000, 1000 );	
+	            var skyGeometry = new THREE.CubeGeometry( 7430, 7430, 1000 );	
 	
 	            var materialArray = [];
 	            for (var i = 0; i < 6; i++)
@@ -15,13 +15,12 @@ class Skybox extends THREE.Group {
 	            var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
 				var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
 				mskyBox.add(skyBox);
-				//scene.add( skyBox );
+				
 			}
 
     
 			constructor(){
 				super();
-				//this._LoadState = LoadStates.NOT_LOADING;
 				this.init();
 			}	
         }
