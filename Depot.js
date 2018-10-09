0ab83bf0ec4dcde8b487d25ae14294a50102ffa3
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-09 10:34:02 +0200

    good texture and position
    good texture and position

--- Depot.js
+++ Depot.js
@@ -4,18 +4,18 @@ class Depot extends THREE.Group {
         var mdepot = this;
         var dgeometry = new THREE.BoxGeometry(30, 60, 30);
         var dcubeMaterials = [
-            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //LEFT
-            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //RIGHT
-            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bobdebouwer.png"), side: THREE.DoubleSide }), //TOP
-            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //BOTTOM
-            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bobdebouwer.png"), side: THREE.DoubleSide }), //FRONT
-            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide }), //BACK
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //LEFT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //RIGHT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //TOP
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //BOTTOM
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //FRONT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/bricks.jpg"), side: THREE.DoubleSide }), //BACK
             ];
         var dmaterial = new THREE.MeshFaceMaterial(dcubeMaterials);
         var ddepot = new THREE.Mesh(dgeometry, dmaterial);
-        ddepot.position.x = 7;
+        ddepot.position.x = 47;
         ddepot.position.y = 0;
-        ddepot.position.z = 7;
+        ddepot.position.z = 47;
         mdepot.add(ddepot);   
     }
 
