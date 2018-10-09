20bde50870f19529425a1363f19a19fc42b0dec8
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-09 13:14:50 +0200

    plane met texture
    plane met texture

--- Plane.js
+++ Plane.js
@@ -1,17 +1,20 @@
 class Plane extends THREE.Group {
-	init(){
-                var mPlane = this;
-                var material        = new THREE.MeshBasicMaterial({ color:0x008000 })
-                var geometry        = new THREE.PlaneGeometry( 7430, 7430 )
-                var plane        = new THREE.Mesh( geometry, material );
-                plane.rotation.x= - 90 * Math.PI / 180;
-                //plane.rotation.x = Math.PI / 2.0;
-                //plane.position.x = 15;
-                //plane.position.z = 15;
-                mPlane.add(plane);
-    }
-    constructor(){
+    init(){
+            var plane, geometry, material, mPlane, texture;
+
+            mPlane = this;
+            texture = new THREE.TextureLoader().load( "textures/plane/ground_texture3.jpg" );
+            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+            texture.repeat.set( 200, 200 );
+            material = new THREE.MeshBasicMaterial( { map: texture} );
+    //material = new THREE.MeshBasicMaterial({ color:0x008000 });
+    geometry = new THREE.PlaneGeometry( 7430, 7430 );
+    plane = new THREE.Mesh( geometry, material );
+    plane.rotation.x= - 90 * Math.PI / 180;
+    mPlane.add(plane);
+}
+constructor(){
         super();
-        this.init();
-    }	
+    this.init();
+}
 }
