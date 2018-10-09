f2b736641e8b47ab88ee40a87a11ab6d2aa2d8a1
RicardoBakker <rbakkiebakkie@gmail.com>
2018-10-09 11:56:38 +0200

    + Plane Texture
    Added texture loader.
    Added texture repeater.

---
+++ Plane.js
@@ -0,0 +1,20 @@
+class Plane extends THREE.Group {
+		init(){
+				var plane, geometry, material, mPlane, texture;
+
+				mPlane = this;
+				texture = new THREE.TextureLoader().load( "textures/Plane/ground_texture5.jpg" );
+				texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+				texture.repeat.set( 200, 200 );
+				material = new THREE.MeshBasicMaterial( { map: texture} );
+        //material = new THREE.MeshBasicMaterial({ color:0x008000 });
+        geometry = new THREE.PlaneGeometry( 7430, 7430 );
+        plane = new THREE.Mesh( geometry, material );
+        plane.rotation.x= - 90 * Math.PI / 180;
+        mPlane.add(plane);
+    }
+    constructor(){
+    		super();
+        this.init();
+    }
+}
