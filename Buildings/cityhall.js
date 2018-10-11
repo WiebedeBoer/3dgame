14d763f28d8c6d815580b2c51057060bd6ef65d8
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-11 11:30:27 +0200

    new cityhall
    new cityhall

--- Buildings/cityhall.js
+++ Buildings/cityhall.js
@@ -1,76 +1,53 @@
-var THREEx = THREEx || {}
-THREEx.ProceduralCityHall = function(){
+class Cityhall extends THREE.Group {
+
+    init (){
+
+        var mCityhall = this;
 
 //generate material
 var material19 = new THREE.MeshLambertMaterial({
 color: 0xf0f0f0
 });
 
-//city mesh
-var cityhallGeometry = new THREE.Geometry();
+    //city mesh
+    var townGeometry = new THREE.Geometry();
 
-// build the base geometry for each building
-var geo19 = new THREE.CubeGeometry( 1, 1, 1 );
-
-// translate the geometry to place the pivot point at the bottom instead of the center
-geo19.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
-
-geo19.faceVertexUvs[0][0] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][1] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][2] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][3] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][4] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][5] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][6] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][7] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][8] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][9] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][10] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][11] = [ [1], [2], [3] ];
-
-// buildMesh
-var buildMesh19 = new THREE.Mesh( geo19  );
+    var hallGeometry = new THREE.BoxGeometry(60, 100, 60);
+    var hallMaterials = [
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/concrete.jpg"), side: THREE.DoubleSide }), //LEFT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/concrete.jpg"), side: THREE.DoubleSide }), //RIGHT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/ground11.jpg"), side: THREE.DoubleSide }), //TOP
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/concrete.jpg"), side: THREE.DoubleSide }), //BOTTOM
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/concrete.jpg"), side: THREE.DoubleSide }), //FRONT
+            new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/concrete.jpg"), side: THREE.DoubleSide }), //BACK
+        ];
+    var hallMaterial = new THREE.MeshFaceMaterial(hallMaterials);
+    var meshCityhall = new THREE.Mesh(hallGeometry, material19);
 
 var chcol =32;
 var chrow =30;
 
 // put a position
-buildMesh19.position.x = (2700 - 5400) + (chcol * 90) - 30;
-buildMesh19.position.z = (2700 - 5400) + (chrow * 90) - 30;
+meshCityhall.position.x = (2700 - 5400) + (chcol * 90) - 30;
+meshCityhall.position.z = (2700 - 5400) + (chrow * 90) - 30;
 //put a rotation
-buildMesh19.rotation.y = 0.5*Math.PI*2;
-//building scale
-buildMesh19.scale.x = 60;
-buildMesh19.scale.y = 100;
-buildMesh19.scale.z = buildMesh19.scale.x;
+meshCityhall.rotation.y = 0.5*Math.PI*2;
 
 //add geometry
-var geo19 = buildMesh19.geo19;
+var hallGeometry = meshCityhall.hallGeometry;
 
-THREE.GeometryUtils.merge( cityhallGeometry, buildMesh19 );
+townGeometry.mergeMesh(meshCityhall);
 
-//return mesh
-var mesh17 = new THREE.Mesh(cityhallGeometry, material19);
-return mesh17;
+var mesh4 = new THREE.Mesh(townGeometry, hallMaterial);
+mCityhall.add(mesh4);
+collidableMeshList.push(mesh4);
+    }
 
-//new building end
-
-//window canvas
-function generateTextureCityHall(){
-// build a small canvas 30x50 and paint it in white
-var tch = document.createElement( 'canvas' );
-tch.width = 60;
-tch.height = 100;
-var context = tch.getContext( '2d' );
-// plain it in white
-context.fillStyle = '#ffffff';
-context.fillRect( 0, 0, 60, 100 );
-
-// then draw the image
-context.drawImage( tch, 0, 0, tch.width, tch.height );
-// return the just built canvas
-return tch;
-}
+constructor(){
+    super();
+   
+    this.init();
+  }
 
 
 }
