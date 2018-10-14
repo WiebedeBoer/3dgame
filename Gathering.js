21a401f87fe6b78419c707ff4c7a9744b40947ee
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-14 16:58:52 +0200

    less warnings
    less warnings cleanup

--- Gathering.js
+++ Gathering.js
@@ -2,6 +2,7 @@
 function propGathering(){
 
     //check collision props
+    var propHit =0;
         //check particle hit
         if (propHit >0){
             //gather prop
--- Scoring.js
+++ Scoring.js
@@ -1,7 +1,7 @@
 //scoring
 function Scoring(){
     //registrate particle hit
-
+    var particleHit =0;
     //check particle hit
     if (particleHit >0){
     //increase score    
