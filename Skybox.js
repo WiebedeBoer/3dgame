20b8ec3966657460ffa36f1982ee592147f2a946
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-14 15:57:44 +0200

    cleaned up skybox
    cleaned up skybox less console warnings

--- Skybox.js
+++ Skybox.js
@@ -9,12 +9,13 @@ class Skybox extends THREE.Group {
 	            var materialArray = [];
 	            for (var i = 0; i < 6; i++)
 		            materialArray.push( new THREE.MeshBasicMaterial({
-			        map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+					map: new THREE.TextureLoader().load( imagePrefix + directions[i] + imageSuffix ),
 			        side: THREE.BackSide
-		            }));
+					}));
+				
 	            var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
 				var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
-				//skyBox.rotation.x= - 90 * Math.PI / 180;
+				
 				mskyBox.add(skyBox);
 				collidableMeshList.push(skyBox);
 			}
