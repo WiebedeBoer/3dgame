810d36a304a2602bbbf80c1075c457890f028993
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-15 18:16:18 +0200

    all lanterns
    all lanterns

--- Lighting.js
+++ Lighting.js
@@ -1,5 +1,5 @@
 function Lighter(){
-    //lighting
+//lighting
 var light = new THREE.HemisphereLight( 0xfffff0, 0x101020, 1 );
 light.position.set( 0.5, 1, 0.25 );
 scene.add( light );
@@ -9,6 +9,10 @@ var lantern = new Lantern(138,1,105); //court 1
 scene.add(lantern);
 var lantern2 = new Lantern(166,1,105); //court 2
 scene.add(lantern2);
+var lantern17 = new Lantern(138,1,185); //court 3
+scene.add(lantern17);
+var lantern18 = new Lantern(166,1,185); //court 4
+scene.add(lantern18);
 
 var lantern3 = new Lantern(-1475,1,-155); //school 4,19
 scene.add(lantern3);
@@ -16,7 +20,7 @@ var lantern4 = new Lantern(1225,1,-605); //school 34,14
 scene.add(lantern4);
 var lantern5 = new Lantern(-395,1,745); //school 16,29 
 scene.add(lantern5);
-var lantern6 = new Lantern(235,1,475); //school 23,26 *
+var lantern6 = new Lantern(235,1,475); //school 23,26
 scene.add(lantern6);
 var lantern7 = new Lantern(595,1,475); //school 27,26  
 scene.add(lantern7);
@@ -39,6 +43,5 @@ var lantern15 = new Lantern(1615,1,-800); //school 38,11
 scene.add(lantern15);
 var lantern16 = new Lantern(-905,1,1720); //school 10,39 
 scene.add(lantern16);
-
 
 }
