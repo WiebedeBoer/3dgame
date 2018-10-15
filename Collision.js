eca5446d28efdc082722b66cc0cab2192fc8b240
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-15 10:16:27 +0200

    cleaned collide
    cleaned collide

--- Collision.js
+++ Collision.js
@@ -1,8 +1,6 @@
 function Collide (){
 
 var mcdelta = clock.getDelta(); // seconds.
-var moveDistance = 100 * mcdelta; // 200 pixels per second
-//var moveDistance = controls.movementSpeed;
 var mcrotateAngle = Math.PI / 2 * mcdelta;   // pi/2 radians (90 degrees) per second
 
 if ( keyboard.pressed("q") )
@@ -10,11 +8,13 @@ MovingCube.rotation.y += mcrotateAngle; //rotate left
 if ( keyboard.pressed("e") )
 MovingCube.rotation.y -= mcrotateAngle; //rotate right
 
+
 MovingCube.position.x = camera.position.x;
-MovingCube.position.y = camera.position.y + 1;
+MovingCube.position.y = camera.position.y;
 MovingCube.position.z = camera.position.z;
 
 var originPoint = MovingCube.position.clone();
+var collisionCheck = false;
 
 clearText();
 
@@ -28,35 +28,17 @@ var directionVector = globalVertex.sub( MovingCube.position );
 var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
 var collisionResults = ray.intersectObjects( collidableMeshList );
 if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ){
-//if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() + 5 ){
-//if ( collisionResults.length > 0 && collisionResults[0].distance < 5){
 appendText(" Hit ");
-//console.log("hit");
-if ( keyboard.pressed("d") || keyboard.pressed("s") ){
-controls.movementSpeed -= controls.movementSpeed + controls.movementSpeed;}
-else if ( keyboard.pressed("a") || keyboard.pressed("w") ){
-//controls.movementSpeed += controls.movementSpeed - controls.movementSpeed - controls.movementSpeed;
-controls.movementSpeed -= controls.movementSpeed + controls.movementSpeed;
+collisionCheck = true;
 }
-//controls.movementSpeed -= controls.movementSpeed;
-//controls.movementSpeed = -100;
-//controls.movementSpeed = 0;
-//console.log(controls.movementSpeed);
-//moveDistance -=moveDistance + moveDistance; //80
-//moveDistance -=controls.movementSpeed;
-//console.log(moveDistance);
-//break;
+
 }
-else {
-        //controls.movementSpeed = controls.movementSpeed + 1;
+
+if (!collisionCheck){
         if ( keyboard.pressed("d") || keyboard.pressed("s") ){
         controls.movementSpeed = 80;}
         else if ( keyboard.pressed("a") || keyboard.pressed("w") ){
         controls.movementSpeed = 80;}
-        //controls.movementSpeed = 100;
-        //moveDistance = moveDistance; 
-        //appendText(MovingCube.position.x+","+MovingCube.position.z+";");
-}
 }
 
 }
