92ab85ce4372a4e18dddd8e74da497baa3439efb
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-15 15:19:04 +0200

    col comment
    prop col comment

--- props/Props.js
+++ props/Props.js
@@ -11,6 +11,7 @@ scene.add(gun4);
 var gun5 = new Gun(-919,6,822); //shop 10,30
 scene.add(gun5);
 //gun collection boxes
+/*
 var guncol = new GunCol(228,6,16); //shop 23,20
 scene.add(guncol);
 var guncol2 = new GunCol(127,6,-161); //shop 22,18
@@ -21,5 +22,6 @@ var guncol4 = new GunCol(1150,6,-1428); //shop 33,4
 scene.add(guncol4);
 var guncol5 = new GunCol(-919,6,822); //shop 10,30
 scene.add(guncol5);
+*/
 
 }
