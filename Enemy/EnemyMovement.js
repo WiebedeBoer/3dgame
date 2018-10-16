f47fbe3c24836c82dd8ed52665e88c08fd6dddf7
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-16 12:06:13 +0200

    new distance check
    new distance check

--- Enemy/EnemyMovement.js
+++ Enemy/EnemyMovement.js
@@ -2,14 +2,19 @@
             function EnemyMovement() {
               var cameraPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
               var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
+              //var enemyCubePosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
               var distance = cameraPosition.distanceTo(enemyPosition);
               //console.log(distance);
-              if(distance < 100){
+              if(distance >10 && distance < 60){
                 enemy.lookAt(camera.position);
                 new TWEEN.Tween(enemy.position)
                     .to(camera.position, 200)
                     .start();
                     TWEEN.update();
+              }
+              else if (distance <10){
+                clearText();
+                appendText(" you died ");
               }
               else {
               }
