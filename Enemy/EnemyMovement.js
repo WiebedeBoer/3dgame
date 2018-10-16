a6b2976c02e105c9c6cf1bd3ba80d0e7d728cdc3
RicardoBakker <rbakkiebakkie@gmail.com>
2018-10-16 10:54:19 +0200

    + Merge master fix

--- Enemy/EnemyMovement.js
+++ Enemy/EnemyMovement.js
@@ -1,16 +1,18 @@
-            // Move enemy camera towards player
-            function EnemyMovement() {
-              var cameraPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
-              var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
-              var distance = cameraPosition.distanceTo(enemyPosition);
-              //console.log(distance);
-              if(distance < 100){
-                enemy.lookAt(camera.position);
-                new TWEEN.Tween(enemy.position)
-                    .to(camera.position, 200)
-                    .start();
-                    TWEEN.update();
-              }
-              else {
-              }
-            }
+// Move enemy camera towards player
+function EnemyMovement() {
+    var cameraPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
+    var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
+    var distance = cameraPosition.distanceTo(enemyPosition);
+    if(distance < 100){
+      enemy.lookAt(camera.position);
+      new TWEEN.Tween(enemy.position)
+          .to(camera.position, 100)
+          .start();
+          TWEEN.update();
+    }
+    else {
+    }
+     
+    // console.log(distance);
+
+}
--- Enemy/enemy.js
+++ Enemy/enemy.js
@@ -1,27 +1,20 @@
 class Enemy extends THREE.Group {
     init(){
         var refEnemy, enemy;
-
         refEnemy = this;
-
         enemy = new THREE.Group();
-
         loadOBJModel("Models/Dummy/", "model.obj", "Models/Dummy/", "materials.mtl", (mesh) => {
             mesh.scale.set(10, 10, 10);
             enemy.add(mesh);
         });
-
-        enemy.position.x = this.pX;
-        enemy.position.y = this.pY;
-        enemy.position.z = this.pZ;
         enemy.rotation.y = 90 * Math.PI / 180;
         refEnemy.add(enemy);
     }
     constructor(pX,pY,pZ){
         super();
-        this.pX = pX;
-        this.pY = pY;
-        this.pZ = pZ;
+        this.position.x = pX;
+        this.position.y = pY;
+        this.position.z = pZ;
         this.init();
     }
 }
