070395361f3d6c019ab1ebf9c14430389d9a7195
RicardoBakker <rbakkiebakkie@gmail.com>
2018-10-16 10:39:21 +0200

    Revert "Re-Merged w/ Master"
    This reverts commit 75afd6b290e2bca6536a12093c91a16788b2d376.

--- Enemy/EnemyMaker.js
+++
@@ -1,9 +0,0 @@
-function EnemyMaker() {
-// Enemies
-var enemy = new Enemy(-53,10,7);
-scene.add(enemy);
-var enemy2 = new Enemy(-57,10,106);
-scene.add(enemy2);
-var enemy3 = new Enemy(440,10,13);
-scene.add(enemy3);
-}
--- Pointlight.js
+++
@@ -1,5 +0,0 @@
-function addPointLight(object,color,x,y,z,intensity,distance){
-    var light = new THREE.PointLight(color, intensity, distance);
-    light.position.set(x,y,z);
-    object.add(light);
-}
