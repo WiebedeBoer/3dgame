75afd6b290e2bca6536a12093c91a16788b2d376
RicardoBakker <rbakkiebakkie@gmail.com>
2018-10-16 10:28:10 +0200

    Re-Merged w/ Master

--- Buildings/cityhall.js
+++ Buildings/cityhall.js
@@ -1,76 +1,62 @@
-var THREEx = THREEx || {}
-THREEx.ProceduralCityHall = function(){
+class Cityhall extends THREE.Group {
 
-//generate material
-var material19 = new THREE.MeshLambertMaterial({
-color: 0xf0f0f0
-});
+    init (){
 
-//city mesh
-var cityhallGeometry = new THREE.Geometry();
+        var mCityhall = this;
+    
+    //city geometry
+    var cityGeometry= new THREE.Geometry();
 
-// build the base geometry for each building
-var geo19 = new THREE.CubeGeometry( 1, 1, 1 );
+    //city mesh
+    var hallGeometry = new THREE.CubeGeometry(1, 1, 1 );
 
-// translate the geometry to place the pivot point at the bottom instead of the center
-geo19.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
+    // translate the geometry to place the pivot point at the bottom instead of the center
+    hallGeometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
 
-geo19.faceVertexUvs[0][0] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][1] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][2] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][3] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][4] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][5] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][6] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][7] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][8] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][9] = [ [1], [2], [3] ];
-geo19.faceVertexUvs[0][10] = [ [0], [1], [3] ];
-geo19.faceVertexUvs[0][11] = [ [1], [2], [3] ];
+    //material
+    var hallMaterials = [
+        new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/"+this.leftTex+".jpg"), side: THREE.FrontSide }), //LEFT
+        new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/"+this.rightTex+".jpg"), side: THREE.FrontSide }), //RIGHT
+        new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/ground10.jpg"), side: THREE.FrontSide }), //TOP
+        new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/ground3.jpg"), side: THREE.FrontSide }), //BOTTOM
+        new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/"+this.frontTex+".jpg"), side: THREE.FrontSide }), //FRONT
+        new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load("textures/ground/"+this.backTex+".jpg"), side: THREE.FrontSide }), //BACK
+        ];
+    var hallMaterial = new THREE.MeshFaceMaterial(hallMaterials);
+    var meshCityhall = new THREE.Mesh(hallGeometry);
 
-// buildMesh
-var buildMesh19 = new THREE.Mesh( geo19  );
+    // put a position
+    meshCityhall.position.x = (1800 - 3600) + (this.ncol * 90) - 30;
+    meshCityhall.position.z = (1800 - 3600) + (this.nrow * 90) - 30;
+    //put a rotation
+    meshCityhall.rotation.y = 0.5*Math.PI*2;
+    //building scale
+    meshCityhall.scale.x = 60;
+    meshCityhall.scale.y = this.bHeight;
+    meshCityhall.scale.z = 60;
 
-var chcol =32;
-var chrow =30;
+    // merge it with cityGeometry - very important for performance
+    var hallGeometry = meshCityhall.hallGeometry;
+    cityGeometry.mergeMesh(meshCityhall);
+    //add to class
+    var mesh4 = new THREE.Mesh(cityGeometry, hallMaterial);
+    mCityhall.add(mesh4);
+    //add to collision
+    collidableMeshList.push(mesh4);
+    
+    }
 
-// put a position
-buildMesh19.position.x = (2700 - 5400) + (chcol * 90) - 30;
-buildMesh19.position.z = (2700 - 5400) + (chrow * 90) - 30;
-//put a rotation
-buildMesh19.rotation.y = 0.5*Math.PI*2;
-//building scale
-buildMesh19.scale.x = 60;
-buildMesh19.scale.y = 100;
-buildMesh19.scale.z = buildMesh19.scale.x;
-
-//add geometry
-var geo19 = buildMesh19.geo19;
-
-THREE.GeometryUtils.merge( cityhallGeometry, buildMesh19 );
-
-//return mesh
-var mesh17 = new THREE.Mesh(cityhallGeometry, material19);
-return mesh17;
-
-//new building end
-
-//window canvas
-function generateTextureCityHall(){
-// build a small canvas 30x50 and paint it in white
-var tch = document.createElement( 'canvas' );
-tch.width = 60;
-tch.height = 100;
-var context = tch.getContext( '2d' );
-// plain it in white
-context.fillStyle = '#ffffff';
-context.fillRect( 0, 0, 60, 100 );
-
-// then draw the image
-context.drawImage( tch, 0, 0, tch.width, tch.height );
-// return the just built canvas
-return tch;
-}
+    constructor(frontTex,backTex,rightTex,leftTex,bHeight,crow,ccol){
+    super();
+    this.frontTex = frontTex;
+    this.backTex = backTex;
+    this.rightTex = rightTex;
+    this.leftTex = leftTex;
+    this.bHeight = bHeight;
+    this.nrow = crow;
+    this.ncol = ccol;
+    this.init();
+    }
 
 
 }
---
+++ props/Props.js
@@ -0,0 +1,32 @@
+function Props(){
+//gun objects
+var gun = new Gun(228,6,16); //shop 23,20
+scene.add(gun);
+var gun2 = new Gun(127,6,-161); //shop 22,18
+scene.add(gun2);
+var gun3 = new Gun(-457,6,-1433); //shop 15,4
+scene.add(gun3);
+var gun4 = new Gun(1150,6,-1428); //shop 33,4
+scene.add(gun4);
+var gun5 = new Gun(-919,6,822); //shop 10,30
+scene.add(gun5);
+//gun collection boxes
+var guncol = new GunCol(228,6,16); //shop 23,20
+scene.add(guncol);
+var guncol2 = new GunCol(127,6,-161); //shop 22,18
+scene.add(guncol2);
+var guncol3 = new GunCol(-457,6,-1433); //shop 15,4
+scene.add(guncol3);
+var guncol4 = new GunCol(1150,6,-1428); //shop 33,4
+scene.add(guncol4);
+var guncol5 = new GunCol(-919,6,822); //shop 10,30
+scene.add(guncol5);
+//ambulances
+//var ambulance = new Ambulance(175,1,548); //hospital 22,26
+//scene.add(ambulance);
+//var ambulance2 = new Ambulance(175,1,548); //hospital 11,12
+//scene.add(ambulance2);
+//var ambulance3 = new Ambulance(175,1,548); //hospital 5,33
+//scene.add(ambulance3);
+
+}
