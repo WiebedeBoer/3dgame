9aa23b09eb0af8e05547fb64b1f263c2ccdac44d
RicardoBakker <rbakkiebakkie@gmail.com>
2018-10-16 12:16:42 +0200

    +

--- Enemy/EnemyMovement.js
+++ Enemy/EnemyMovement.js
@@ -3,7 +3,7 @@ function EnemyMovement() {
     var cameraPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
     var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
     var distance = cameraPosition.distanceTo(enemyPosition);
-    if(distance < 100){
+    if(distance < 200){
       enemy.lookAt(camera.position);
       new TWEEN.Tween(enemy.position)
           .to(camera.position, 100)
