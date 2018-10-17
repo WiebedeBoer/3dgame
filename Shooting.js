9b7e9c37b897a4072253c3ab40a053f5115b0180
Unknown <silhutting@hotmail.com>
2018-10-17 11:54:37 +0200

    clickfix voor muis beweging

--- Shooting.js
+++ Shooting.js
@@ -8,17 +8,13 @@ function Shot(){
             var ammoGeometry = new THREE.CubeGeometry(3,3,3,1,1,1);
             var ammoMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe:true, visible:true } );
             var ammoCube = new THREE.Mesh( ammoGeometry, ammoMaterial );
+            
             //set particle position
             ammoCube.position.set(camera.position.x, camera.position.y - 3, camera.position.z);
             //set rotation to that of the camera (player)
             ammoCube.rotation.x = camera.rotation.x;
             ammoCube.rotation.y = camera.rotation.y;
             ammoCube.rotation.z = camera.rotation.z;
-
-            // set the velocity of the bullet
-            //ammoCube.velocity = new THREE.Vector3(-Math.sin(80),0,Math.cos(80));
-            
-            //ammoCube.position.add(ammoCube.velocity);
 
             //add particle
             scene.add( ammoCube );
