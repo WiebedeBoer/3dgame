ab529cb1177ba6acb3401051a96d39817e234138
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-17 13:25:04 +0200

    enemy moving en cube
    enemy moving en cube

--- Enemy/EnemyMovement.js
+++ Enemy/EnemyMovement.js
@@ -1,26 +1,29 @@
-            // Move enemy camera towards player
-            function EnemyMovement() {
-              var cameraPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
-              var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
-              //var enemyCubePosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
-              var distance = cameraPosition.distanceTo(enemyPosition);
-              //console.log(distance);
-              if(distance >10 && distance < 60){
-                enemy.lookAt(camera.position);
-                new TWEEN.Tween(enemy.position)
-                    .to(camera.position, 200)
-                    .start();
-                    TWEEN.update();
-              }
-              else if (distance <10){
-                clearText();
-                appendText(" you died ");
-                var gameover = document.getElementById("menu");
-                if (gameover.style.display === "none") {
-                    gameover.style.display = "block";} 
-                    //else {
-                    //gameover.style.display = "none";}
-              }
-              else {
-              }
-            }
+function EnemyMovement() {
+
+  var cameraPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
+  enemyList.forEach(function(enemy) {
+      
+      var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
+      var distance = cameraPosition.distanceTo(enemyPosition);
+      console.log(enemy.uuid);
+
+      if(distance > 10 && distance < 100){
+        enemy.lookAt(cameraPosition);
+        enemy.translateZ(0.3);
+      }else if (distance < 10){
+          clearText();
+          appendText("you died");
+          if (gameover.style.display === "none") {
+            gameover.style.display = "block";} 
+      }
+      //Relocate the cube to the enemy at all times.
+      enemy.enemyCube.position.x = enemy.position.x;
+      enemy.enemyCube.position.y = enemy.position.y;
+      enemy.enemyCube.position.z = enemy.position.z;
+      enemy.enemyCube.rotation.x = enemy.rotation.x;
+      enemy.enemyCube.rotation.y = enemy.rotation.y;
+      enemy.enemyCube.rotation.z = enemy.rotation.z;
+  });
+}     
+            
+            
