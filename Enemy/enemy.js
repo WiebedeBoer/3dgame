51fd7fdd22e47101499e0d85fd4c55ba6b66e697
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-18 14:42:24 +0200

    enemycube hidden
    enemycube hidden

--- Enemy/enemy.js
+++ Enemy/enemy.js
@@ -1,28 +1,19 @@
 class Enemy extends THREE.Group {
-    init(){
-        
+    init(){        
         var refEnemy, enemy;
-
         refEnemy = this;
-
         enemy = new THREE.Group();
-
         loadOBJModel("Models/Dummy/", "model.obj", "Models/Dummy/", "materials.mtl", (mesh) => {
             mesh.scale.set(10, 10, 10);
             enemy.add(mesh);
         });
-
         enemy.rotation.y = 90 * Math.PI / 180;
-        refEnemy.add(enemy);
-        
+        refEnemy.add(enemy);        
         this.enemyCubeGeometry = new THREE.CubeGeometry(10,10,10,1,1,1);
-        var enemyCubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe:true, visible:true } );
-        this.enemyCube = new THREE.Mesh( this.enemyCubeGeometry, enemyCubeMaterial );
-        //enemy.add(this.enemyCube);
+        var enemyCubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe:true, visible:false } );
+        this.enemyCube = new THREE.Mesh( this.enemyCubeGeometry, enemyCubeMaterial );        
         this.position.set(this.position.x, this.position.y, this.position.z);
-        this.enemyCube.position.set(this.position.x, this.position.y, this.position.z);    
-
-        
+        this.enemyCube.position.set(this.position.x, this.position.y, this.position.z); 
     }
 
     constructor(pX,pY,pZ){
@@ -31,8 +22,7 @@ class Enemy extends THREE.Group {
         this.position.y = pY;
         this.position.z = pZ;
         this.init();
-        //Creating and locating the HitboxCube of the enemy.
-        //this.enemyCube = new EnemyCube(pX, pY, pZ);
+        //Creating and locating the HitboxCube of the enemy.        
         scene.add(this.enemyCube);
     }
 }
