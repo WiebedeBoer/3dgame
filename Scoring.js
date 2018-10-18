fd565268f9f4e9cf65243528f2e3ce7ddb7596b6
Unknown <silhutting@hotmail.com>
2018-10-18 11:18:07 +0200

    Alle enemies kunnen worden gekilled
    nu nog de kogel verwijderen

--- Scoring.js
+++ Scoring.js
@@ -1,45 +1,35 @@
 //scoring
 function Scoring(){
-    //registrate particle hit
-    var particleHit =false;
     //particle collision
-    var originEnemyPoint = enemy.position.clone();
     
-    for (var vertexBulletIndex = 0; vertexBulletIndex < enemy.enemyCubeGeometry.vertices.length; vertexBulletIndex++)
-    {
-        var localVertex = enemy.enemyCubeGeometry.vertices[vertexBulletIndex].clone();
-        var globalVertex = localVertex.applyMatrix4( enemy.matrix );
-        var directionVector = globalVertex.sub( enemy.position );
+    enemyList.forEach(enemyResult => {
+        var originEnemyPoint = enemyResult.position.clone();
+        for (var vertexBulletIndex = 0; vertexBulletIndex < enemyResult.enemyCubeGeometry.vertices.length; vertexBulletIndex++)
+        {
+            var localVertex = enemyResult.enemyCubeGeometry.vertices[vertexBulletIndex].clone();
+            var globalVertex = localVertex.applyMatrix4( enemyResult.matrix );
+            var directionVector = globalVertex.sub( enemyResult.position );
+    
+            var rayBullet = new THREE.Raycaster( originEnemyPoint, directionVector.clone().normalize() );
+            var collisionResults = rayBullet.intersectObjects( collidableBulletMeshList );
+            //check collision bullets
+            if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ){
+                console.log(collisionResults);
 
-        var rayBullet = new THREE.Raycaster( originEnemyPoint, directionVector.clone().normalize() );
-        var collisionResults = rayBullet.intersectObjects( collidableBulletMeshList );
-        //check collision bullets
-        if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ){
-            clearText();
-            appendText(" kill ");
-            particleHit = true;
-        }
-    }    
-    
-    //check particle hit
-    if (particleHit ==true){
-    //increase score    
-    totalscore = totalscore + 1;
-    //update score
-    eScore ="Score: "+totalscore;
-    document.getElementById('score').innerHTML = eScore; 
-    particleHit = false;
-    //kill ammo cube
-    let pos = bullets.indexOf(ammoCube.uuid);
-    ammoCube.alive = false;
-    bullets.splice(pos,1);
-    collidableBulletMeshList.splice(pos,1);
-    scene.remove(ammoCube);
-    //kill enemy cube
-    scene.remove(enemy.enemyCube);
-    //kill enemy
-    scene.remove(enemy);
-    }
-    //console.log(particleHit);
+                clearText();
+                appendText(" kill ");
+                
+                //increase score    
+                totalscore = totalscore + 1;
+                //update score
+                eScore ="Score: "+totalscore;
+                document.getElementById('score').innerHTML = eScore; 
+                
+                //Remove enemy
+                scene.remove(enemyResult.enemyCube);
+                scene.remove(enemyResult);
+            }
+        }    
+    });
     
 }
