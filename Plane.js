541b99040b1b8b116ae3db582634097726e871d6
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-18 14:11:35 +0200

    new plane class
    new plane class

--- Plane.js
+++ Plane.js
@@ -1,20 +1,24 @@
 class Plane extends THREE.Group {
     init(){
-            var plane, geometry, material, mPlane, texture;
+        var plane, geometry, material, mPlane, texture;
 
-            mPlane = this;
+        mPlane = this;
+        
             texture = new THREE.TextureLoader().load( "textures/plane/ground_texture3.jpg" );
             texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
             texture.repeat.set( 200, 200 );
             material = new THREE.MeshBasicMaterial( { map: texture} );
-    //material = new THREE.MeshBasicMaterial({ color:0x008000 });
-    geometry = new THREE.PlaneGeometry( 5630, 5630 );
-    plane = new THREE.Mesh( geometry, material );
-    plane.rotation.x= - 90 * Math.PI / 180;
-    mPlane.add(plane);
-}
-constructor(){
+        
+      
+        geometry = new THREE.PlaneGeometry( this.width, this.length );
+        plane = new THREE.Mesh( geometry, material );
+        plane.rotation.x= - 90 * Math.PI / 180;
+        mPlane.add(plane);
+    }
+    constructor(width,length){
         super();
-    this.init();
-}
+        this.width = width;
+        this.length = length;
+        this.init();
+    }
 }
