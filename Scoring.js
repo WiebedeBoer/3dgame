f933c69d5c7dd4bd4f9891a7dcebad1cd15acf4e
RicardoBakker <rbakkiebakkie@gmail.com>
2018-10-19 10:47:42 +0200

    Merged w/ Master

--- Scoring.js
+++ Scoring.js
@@ -1,45 +1,39 @@
-// Scoring
+//scoring
 function Scoring(){
+    //particle collision
+    
+    enemyList.forEach(enemyResult => {
+        var originEnemyPoint = enemyResult.position.clone();
+        for (var vertexBulletIndex = 0; vertexBulletIndex < enemyResult.enemyCubeGeometry.vertices.length; vertexBulletIndex++)
+        {
+            var localVertex = enemyResult.enemyCubeGeometry.vertices[vertexBulletIndex].clone();
+            var globalVertex = localVertex.applyMatrix4( enemyResult.matrix );
+            var directionVector = globalVertex.sub( enemyResult.position );
+    
+            var rayBullet = new THREE.Raycaster( originEnemyPoint, directionVector.clone().normalize() );
+            var collisionResults = rayBullet.intersectObjects( collidableBulletMeshList );
+            //check collision bullets
+            if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ){
+                clearText();
+                appendText(" kill ");
 
-    // Registrate particle hit
-    var particleHit = false;
+                //increase score    
+                totalscore = totalscore + 1;
+                //update score
+                eScore ="Score: "+totalscore;
+                document.getElementById('score').innerHTML = eScore;
 
-    // Particle collision
-    var originEnemyPoint = enemyCube.position.clone();
-
-    for (var vertexBulletIndex = 0; vertexBulletIndex < enemyCube.enemyCubeGeometry.vertices.length; vertexBulletIndex++)
-    {
-        var localVertex = enemyCube.enemyCubeGeometry.vertices[vertexBulletIndex].clone();
-        var globalVertex = localVertex.applyMatrix4( enemyCube.matrix );
-        var directionVector = globalVertex.sub( enemyCube.position );
-        var rayBullet = new THREE.Raycaster( originEnemyPoint, directionVector.clone().normalize() );
-        var collisionResults = rayBullet.intersectObjects( collidableBulletMeshList );
-        // check collision bullets
-        if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) {
-            clearText();
-            appendText(" kill ");
-            particleHit = true;
-        }
-    }
-
-    //check particle hit
-    if (particleHit ==true){
-    //increase score
-    totalscore = totalscore + 1;
-    //update score
-    eScore ="Score: "+totalscore;
-    document.getElementById('score').innerHTML = eScore;
-    particleHit = false;
-    //kill ammo cube
-    //ammoCube.alive = false;
-    //scene.remove(ammoCube);
-    //kill enemy cube
-    //enemyCube.alive = false;
-    //scene.remove(enemyCube);
-    //kill enemy
-    //enemy.alive = false;
-    //scene.remove(enemy);
-    }
-    console.log(particleHit);
-
+                //Remove enemy and bullet from their respective arrays
+                let indexBullet = bullets.map(e => e.uuid).indexOf(collisionResults[0]['object']['uuid']);
+                bullets.splice(indexBullet,1);
+                let indexEnemy = enemyList.map(e => e.uuid).indexOf(enemyResult.uuid);
+                enemyList.splice(indexEnemy,1);
+                scene.remove(collisionResults[0]['object']);
+                //Remove enemy and bullet from the scene
+                scene.remove(enemyResult.enemyCube)
+                scene.remove(enemyResult);
+            }
+        }    
+    });
+    
 }
