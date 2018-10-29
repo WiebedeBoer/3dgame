955de9deafff3f04680b2767ed5e842f08fd7fac
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-29 13:05:03 +0100

    add ids menu
    add ids menu

--- Scoring.js
+++ Scoring.js
@@ -1,7 +1,7 @@
 //scoring
 function Scoring(){
     //particle collision
-
+    
     enemyList.forEach(enemyResult => {
         var originEnemyPoint = enemyResult.position.clone();
         for (var vertexBulletIndex = 0; vertexBulletIndex < enemyResult.enemyCubeGeometry.vertices.length; vertexBulletIndex++)
@@ -9,7 +9,7 @@ function Scoring(){
             var localVertex = enemyResult.enemyCubeGeometry.vertices[vertexBulletIndex].clone();
             var globalVertex = localVertex.applyMatrix4( enemyResult.matrix );
             var directionVector = globalVertex.sub( enemyResult.position );
-
+    
             var rayBullet = new THREE.Raycaster( originEnemyPoint, directionVector.clone().normalize() );
             var collisionResults = rayBullet.intersectObjects( collidableBulletMeshList );
             //check collision bullets
@@ -17,31 +17,29 @@ function Scoring(){
                 clearText();
                 appendText(" kill ");
                 //play sound
-                var aud = document.getElementById("soundpar");
-                aud.play();
-                //increase score
+                var aud = document.getElementById("soundpar"); 
+                aud.play(); 
+                //increase score    
                 totalscore = totalscore + 1;
                 //update score
                 appendScore();
                 //Remove enemy and bullet from their respective arrays
-                //remove bullet and bullet mesh from the scene
+                //remove bullet and bullet mesh from the scene  
                 let indexBullet = bullets.map(e => e.uuid).indexOf(collisionResults[0]['object']['uuid']);
-                bullets.splice(indexBullet,1);
+                bullets.splice(indexBullet,1);                              
                 scene.remove(collisionResults[0]['object']);
-                //remove from bulletmeshes array
+                //remove from bulletmeshes array   
                 var rem = bulletmeshes.slice(indexBullet,indexBullet+1);
                 scene.remove(rem[0]);
-                bulletmeshes.splice(indexBullet,1);
+                bulletmeshes.splice(indexBullet,1);                      
                 //Remove enemy and enemy cube from the scene
                 let indexEnemy = enemyList.map(e => e.uuid).indexOf(enemyResult.uuid);
                 enemyList.splice(indexEnemy,1);
                 scene.remove(enemyResult.enemyCube);
                 scene.remove(enemyResult);
                 enemyCounter++;
-                console.log('Score counter:' + enemyCounter);
-                console.log('Aantal enemies:' + enemyList.length);
             }
-        }
+        }    
     });
-
+    
 }
