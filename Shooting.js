f92d55c7ecc13c20549f724687d95da6d40e2144
RicardoBakker <rbakkiebakkie@gmail.com>
2018-10-29 11:15:09 +0100

    Revert "Merged with master"
    This reverts commit 5ca4246f5a5430a2f31a4a308b23186023407c5b.

--- Shooting.js
+++ Shooting.js
@@ -4,12 +4,10 @@ function Shot(){
         if (leftOverAmmo >0){
             //less ammo
             leftOverAmmo = leftOverAmmo - 1;
-            
             //create ammo particle
             var ammoGeometry = new THREE.CubeGeometry(3,3,3,1,1,1);
-            var ammoMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe:true, visible:false } );
+            var ammoMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe:true, visible:true } );
             var ammoCube = new THREE.Mesh( ammoGeometry, ammoMaterial );
-
             //set particle position
             ammoCube.position.set(camera.position.x, camera.position.y - 3, camera.position.z);
             //set rotation to that of the camera (player)
@@ -17,24 +15,6 @@ function Shot(){
             ammoCube.rotation.y = camera.rotation.y;
             ammoCube.rotation.z = camera.rotation.z;
             
-            //create bullet mesh
-            var bulletmesh;
-            bulletmesh = new THREE.Group();
-                loadOBJModel("Models/Bullet_obj/", "Bullet.obj", "Models/Bullet_obj/", "Bullet.mtl", (mesh) => {
-                    bulletmesh.add(mesh);
-                    //scale
-                    bulletmesh.scale.set(0.01, 0.01, 0.01);                    
-                    //set position
-                    bulletmesh.position.set(ammoCube.position.x, ammoCube.position.y, ammoCube.position.z);
-                    //set rotation to that of the camera (player)
-                    bulletmesh.rotation.x = camera.rotation.x;
-                    bulletmesh.rotation.y = camera.rotation.y;                                   
-                    bulletmesh.rotation.z = camera.rotation.z;
-                    //push in list
-                    bulletmeshes.push(bulletmesh);
-                    //add to scene
-                    scene.add(bulletmesh);                            
-                });                 
 
             //add particle
             scene.add( ammoCube );
@@ -44,33 +24,25 @@ function Shot(){
             //set timeout bullet
             ammoCube.alive = true;
             setTimeout(function(){
-                let indexBullet = bullets.map(e => e.uuid).indexOf(ammoCube.uuid);                
+                let indexBullet = bullets.map(e => e.uuid).indexOf(ammoCube.uuid);
                 if(indexBullet !== -1){
                     bullets.splice(indexBullet,1);
-                    scene.remove(ammoCube);                    
+                    scene.remove(ammoCube);
                 }
-                let indexBulletmesh = bulletmeshes.map(m => m.uuid).indexOf(bulletmesh.uuid);
-                if(indexBulletmesh !== -1){
-                    bulletmeshes.splice(indexBullet,1);                    
-                    scene.remove(bulletmesh);
-                }
-            }, 3000);
+            }, 10000);
 
         }
         else {
             leftOverAmmo = 0;  
         }
-        //update ammo in HUD
-        appendAmmo();
+        //display ammo in HUD
+        eAmmo = leftOverAmmo+"/"+totalAmmo;        
+        document.getElementById('ammo').innerHTML = eAmmo;
     
 } 
-//move bullet cube
+
 function BulletTravel(){
     bullets.forEach(element => {
-        element.translateZ(-10);
-    });
-    bulletmeshes.forEach(element => {
-        element.translateZ(-10); 
+        element.translateZ( -2 );
     });
 }
-
