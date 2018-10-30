cac06f2c4240c4a711c8fe13e606ccbdabb7be73
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-30 11:52:41 +0100

    nodes
    nodes

--- Graph.js
+++ Graph.js
@@ -1,8 +1,23 @@
 //graph
-function Graph(){
+class Graph{
 //create graph
 
+init(){  
+    var nodeX = (1800 - 3600) + (this.positionX * 90) - 75;
+    var nodeY = this.positionY;
+    var nodeZ = (1800 - 3600) + (this.positionZ * 90) - 75;
+    this.position.set(nodeX,nodeY,nodeZ);
+}
 
 //graph coords
+
+
+    constructor(pX,pY,pZ,name){    
+        this.positionX = pX;
+        this.positionY = pY;
+        this.positionZ = pZ;
+        this.name =name;
+        this.init();
+    }
 
 }
