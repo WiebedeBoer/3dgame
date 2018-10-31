27873aa23c8351290e3f43a05d8224dfc0190b5b
Unknown <silhutting@hotmail.com>
2018-10-31 13:14:31 +0100

    Beginseltje Dijkstra

--- Enemy/EnemyMovement.js
+++ Enemy/EnemyMovement.js
@@ -1,15 +1,20 @@
 function EnemyMovement() {
 
   var cameraPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
+  var nodeClosestToPlayer = GetClosestNodeToPlayer();
+  
   enemyList.forEach(function(enemy) {
       
       var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
       var distance = cameraPosition.distanceTo(enemyPosition);
-      //console.log(enemy.uuid);
 
-      if(distance > 10 && distance < 100){
+      if(distance > 10 && distance < 50){
         enemy.lookAt(cameraPosition);
         enemy.translateZ(0.3);
+      }else if (distance >= 50){
+        //Player to far away, Now follow the player using the created nodes.
+        //First we have to know our closest node, and make sure the player isent super far away.
+        
       }else if (distance < 10){
           clearText();
           appendText("you died");
@@ -30,4 +35,43 @@ function EnemyMovement() {
   });
 }     
             
-            
+//Returns a Graph node found in the MyNodes array, the one closest to the player.
+function GetClosestNodeToPlayer(){
+  let currentPos = new THREE.Vector3( camera.position.x, camera.position.y, camera.position.z );
+  let closestNode;
+  let shortestDistance;
+  
+  MyNodes.forEach(element => {
+          let currentNodePos = new THREE.Vector3 ( element.positionX, element.positionY, element.positionZ );
+          let distance = currentPos.distanceTo(currentNodePos);
+
+          if (shortestDistance == undefined){
+              shortestDistance = distance; 
+              closestNode = element;
+          }else if (distance < shortestDistance) {
+              shortestDistance = distance;
+              closestNode = element;
+          }
+  });
+  return closestNode;
+}
+
+//Returns a Graph node found in the Mynodes array, the one closest to the current enemy
+function GetClosestNodeToEnemy(enemyPosition){
+  let closestNode;
+  let shortestDistance;
+
+  MyNodes.forEach(element=>{
+    let currentNodePos = new THREE.Vector3 ( element.positionX, element.positionY, element.positionZ );
+          let distance = enemyPosition.distanceTo(currentNodePos);
+
+          if (shortestDistance == undefined){
+              shortestDistance = distance; 
+              closestNode = element;
+          }else if (distance < shortestDistance) {
+              shortestDistance = distance;
+              closestNode = element;
+          }
+  });
+  return closestNode
+}
