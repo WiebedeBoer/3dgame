90f71a1249cb274c18aa9ee72e21f260514d759b
Unknown <silhutting@hotmail.com>
2018-11-01 18:59:36 +0100

    Dijkstra werkt
    pathfinding is nog wel non-optimaal.

--- Dijkstra.js
+++ Dijkstra.js
@@ -26,7 +26,7 @@ function PriorityQueue () {
 /**
  * Pathfinding starts here
  */
-function Graph(){
+function Dijkstra(){
   var INFINITY = 1/0;
   this.vertices = {};
 
@@ -88,7 +88,7 @@ function Graph(){
   };
 }
 
-var g = new Graph();
+var g = new Dijkstra();
 /*
 g.addVertex('A', {B: 7, C: 8});
 g.addVertex('B', {A: 7, F: 2});
@@ -124,140 +124,3 @@ g.addVertex('u', {t: 90,v: 90});
 g.addVertex('v', {u: 90,w: 90});
 g.addVertex('w', {v: 90,x: 90});
 g.addVertex('x', {q: 90,w: 90});
-
-// Log test, with the addition of reversing the path and prepending the first node so it's more readable
-console.log(g.shortestPath('A', 'H').concat(['A']).reverse());
-
-
-/*
-//dijkstra solve graph starting at s
-function solve(graph, s) {
-  var solutions = {};
-  solutions[s] = [];
-  solutions[s].dist = 0;
-  
-  while(true) {
-    var parent = null;
-    var nearest = null;
-    var dist = Infinity;
-    
-    //for each existing solution
-    for(var n in solutions) {
-      if(!solutions[n])
-        continue
-      var ndist = solutions[n].dist;
-      var adj = graph[n];
-      //for each of its adjacent nodes...
-      for(var a in adj) {
-        //without a solution already...
-        if(solutions[a])
-          continue;
-        //choose nearest node with lowest *total* cost
-        var d = adj[a] + ndist;
-        if(d < dist) {
-          //reference parent
-          parent = solutions[n];
-          nearest = a;
-          dist = d;
-        }
-      }
-    }
-    
-    //no more solutions
-    if(dist === Infinity) {
-        break;
-    }
-    
-    //extend parent's solution path
-    solutions[nearest] = parent.concat(nearest);
-    //extend parent's cost
-    solutions[nearest].dist = dist;
-  }
-  
-  return solutions;
-}
-//create graph
-var graph = {};
-
-var layout = {
-  'a': ['h','b'],
-  'b': ['a','c'],
-  'c': ['b','d'],
-  'd': ['c','e'],
-  'e': ['d','f'],
-  'f': ['e','g'],
-  'g': ['f','m'],
-  'h': ['a','i'],
-  'i': ['h','j'],
-  'j': ['i','k'],
-  'k': ['j','l'],
-  'l': ['k','r'],
-  'm': ['g','n'],
-  'n': ['m','o'],
-  'o': ['n','p'],
-  'p': ['o','q'],
-  'q': ['p','x'],
-  'r': ['l','s'],
-  's': ['r','t'],
-  't': ['s','u'],
-  'u': ['t','v'],
-  'v': ['u','w'],
-  'w': ['v','x'],
-  'x': ['q','w']
-}
-
-//convert uni-directional to bi-directional graph
-// needs to look like: where: { a: { b: cost of a->b }
-// var graph = {
-//     a: {e:1, b:1, g:3},
-//     b: {a:1, c:1},
-//     c: {b:1, d:1},
-//     d: {c:1, e:1},
-//     e: {d:1, a:1},
-//     f: {g:1, h:1},
-//     g: {a:3, f:1},
-//     h: {f:1}
-// };
-
-for(var id in layout) {
-  if(!graph[id])
-    graph[id] = {};
-  layout[id].forEach(function(aid) {
-    graph[id][aid] = 1;
-    if(!graph[aid])
-      graph[aid] = {};
-    graph[aid][id] = 1;
-  });
-}
-
-//choose start node
-var start = '10';
-//get all solutions
-var solutions = solve(graph, start);
-
-//console.log("From '"+start+"' to");
-//display solutions
-for(var s in solutions) {
-  if(!solutions[s]) continue;
-  //console.log(" -> " + s + ": [" + solutions[s].join(", ") + "]   (dist:" + solutions[s].dist + ")");
-}
-
-// From '10' to
-//  -> 2: [7, 5, 4, 2]   (dist:4)
-//  -> 3: [7, 5, 4, 3]   (dist:4)
-//  -> 4: [7, 5, 4]   (dist:3)
-//  -> 5: [7, 5]   (dist:2)
-//  -> 6: [7, 5, 4, 3, 6]   (dist:5)
-//  -> 7: [7]   (dist:1)
-//  -> 8: [7, 5, 4, 8]   (dist:4)
-//  -> 9: [7, 5, 4, 3, 13, 14, 9]   (dist:7)
-//  -> 10: []   (dist:0)
-//  -> 11: [7, 5, 11]   (dist:3)
-//  -> 12: [7, 5, 11, 12]   (dist:4)
-//  -> 13: [7, 5, 4, 3, 13]   (dist:5)
-//  -> 14: [7, 5, 4, 3, 13, 14]   (dist:6)
-//  -> 15: [7, 5, 4, 3, 6, 15]   (dist:6)
-//  -> R: [7, 5, 4, 2, R]   (dist:5)
-
-*/
-
--- Enemy/EnemyMovement.js
+++ Enemy/EnemyMovement.js
@@ -7,23 +7,44 @@ function EnemyMovement() {
       var enemyPosition = new THREE.Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
       var distance = cameraPosition.distanceTo(enemyPosition);
 
-      if(distance > 90 && distance < 270){
+      if(distance >= 200){
         //travel over nodes
-        //GetClosestNodeToPlayer();
-      }
-      else if(distance > 10 && distance < 90){
-        enemy.lookAt(cameraPosition);
-        enemy.translateZ(0.3);
-      }else if (distance >= 50){
-        if (enemy.pathNodes != null){
-          enemy.lookAt(nodeClosestToPlayer);
-          enemy.translateZ(0.3);
-        }else {
+        //No current path? or current end destination not closest to the Player anymore? change path!
+        if(enemy.pathNodes.length === 0 || enemy.pathNodes == undefined || enemy.pathNodes[enemy.pathNodes.length-1].name !== nodeClosestToPlayer.name){
+          
+          enemy.pathNodes = [];
+          //No current path? create one
+          let closeEnemyNode = GetClosestNodeToEnemy(enemyPosition);
+          //Path of closest node to enemy to the closes node to the player. path will be an array of chars of the respective nodes
+          var path = g.shortestPath(closeEnemyNode.name, nodeClosestToPlayer.name).concat([closeEnemyNode.name]).reverse();
+          //which char belongs to what coordinates?
+          
+          path.forEach(element => {
+            let indexNode = MyNodes.map(e => e.name).indexOf(element);
+            //add node to the path of the enemy
+            enemy.pathNodes.push(new Graph(MyNodes[indexNode].positionX,MyNodes[indexNode].positionY,MyNodes[indexNode].positionZ,MyNodes[indexNode].name));
+          });
 
         }
-        //Player to far away, Now follow the player using the created nodes.
-        //First we have to know our closest node, and make sure the player isent super far away.
+        //If the current enemy is closer to the player than the closest node is closer to the player, then go straight for the player.
+        if(cameraPosition.distanceTo(enemyPosition) < cameraPosition.distanceTo(new THREE.Vector3( enemy.pathNodes[enemy.pathNodes.length-1].positionX, enemy.pathNodes[enemy.pathNodes.length-1].positionY, enemy.pathNodes[enemy.pathNodes.length-1].positionZ ))){
+          enemy.lookAt(new THREE.Vector3(cameraPosition));
+          enemy.translateZ(0.3);
+        }else {
+          //if not, lets go get the player via the path
+          enemy.lookAt(new THREE.Vector3( enemy.pathNodes[0].positionX, enemy.pathNodes[0].positionY, enemy.pathNodes[0].positionZ ));
+          enemy.translateZ(0.3);
+        }
+        //If the distance between the enemy and the next node is less then 1? Then delete that node in our path!
+        if(enemyPosition.distanceTo(new THREE.Vector3( enemy.pathNodes[0].positionX, enemy.pathNodes[0].positionY, enemy.pathNodes[0].positionZ )) < 1){
+          enemy.pathNodes.shift();
+        }
+          
         
+        
+      }else if(distance > 10 && distance < 200){
+        enemy.lookAt(cameraPosition);
+        enemy.translateZ(0.3);
       }else if (distance < 10){
           clearText();
           appendText("you died");
@@ -44,8 +65,7 @@ function EnemyMovement() {
         enemy.enemyCube.rotation.z = enemy.rotation.z;
       
   });
-} 
-
+}
 //Returns a Graph node found in the MyNodes array, the one closest to the player.
 function GetClosestNodeToPlayer(){
   let currentPos = new THREE.Vector3( camera.position.x, camera.position.y, camera.position.z );
@@ -64,6 +84,7 @@ function GetClosestNodeToPlayer(){
               closestNode = element;
           }
   });
+  //return new THREE.Vector3 (closestNode.positionX, closestNode.positionY, closestNode.positionZ);
   return closestNode;
 }
 
@@ -84,50 +105,6 @@ function GetClosestNodeToEnemy(enemyPosition){
               closestNode = element;
           }
   });
+  //return new THREE.Vector3(closestNode.positionX, closestNode.positionY, closestNode.positionZ);
   return closestNode
-}
-            
-//Returns a Graph node found in the MyNodes array, the one closest to the player.
-function GetClosestNodeToPlayer(){
-  let currentPos = new THREE.Vector3( camera.position.x, camera.position.y, camera.position.z );
-  let closestNode;
-  let shortestDistance;
-  
-  MyNodes.forEach(element => {
-          let currentNodePos = new THREE.Vector3 ( element.positionX, element.positionY, element.positionZ );
-          let distance = currentPos.distanceTo(currentNodePos);
-
-          if (shortestDistance == undefined){
-              shortestDistance = distance; 
-              closestNode = element;
-          }else if (distance < shortestDistance) {
-              shortestDistance = distance;
-              closestNode = element;
-          }
-  });
-  //console.log(closestNode);
-  return new THREE.Vector3 (closestNode.positionX, closestNode.positionY, closestNode.positionZ);
- 
-  //return closestNode;
-}
-
-//Returns a Graph node found in the Mynodes array, the one closest to the current enemy
-function GetClosestNodeToEnemy(enemyPosition){
-  let closestNode;
-  let shortestDistance;
-
-  MyNodes.forEach(element=>{
-    let currentNodePos = new THREE.Vector3 ( element.positionX, element.positionY, element.positionZ );
-          let distance = enemyPosition.distanceTo(currentNodePos);
-
-          if (shortestDistance == undefined){
-              shortestDistance = distance; 
-              closestNode = element;
-          }else if (distance < shortestDistance) {
-              shortestDistance = distance;
-              closestNode = element;
-          }
-  });
-  return new THREE.Vector3(closestNode.positionX, closestNode.positionY, closestNode.positionZ);
-  //return closestNode
 }
